182c99ef9d0f39efe7ee297e3bc043496189b7be
rainstriker <cloneltorrer@gmail.com>
2024-05-22 13:12:05 +0700

    feat: Add endpoint for migrating posts
    This commit adds a new endpoint '/posts/migrate' to the post controller. This endpoint allows for migrating posts from an external system to the current system. The migration process involves creating new users, inserting posts, and associating tags with the posts.
    
    The migration logic has been implemented in the postController function. It iterates over the provided data and performs the necessary operations to migrate each post. The logic includes creating new users if they don't exist, inserting posts with the corresponding user and tag associations, and handling any necessary data transformations.

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -10,91 +10,9 @@ export const postController = (es: Elysia) => {
   es.group('/posts', (app) =>
     app
       .use(authenPlugin)
-      .get('/health', ({ authen }) => {
-        console.log('authen', authen)
+      .get('/health', () => {
         return 'Ok'
       })
-      .post(
-        'bulk',
-        async ({ body }) => {
-          const { data } = body as IPosts.BulkBody
-
-          for (const post of data) {
-            post.postedBy
-
-            const res = await db
-              .select()
-              .from(Users)
-              .where(eq(Users.email, post.postedBy))
-              .execute()
-
-            let userId = 0
-            if (res.length === 0) {
-              const newUserRes = await db.insert(Users).values({
-                name: post.postedBy,
-                email: `${post.postedBy}@mockmail.com`,
-                password: await bcrypt.hash(post.postedBy, 10)
-              })
-
-              userId = newUserRes[0].insertId
-            } else {
-              userId = res[0].id
-            }
-
-            const newPostRes = await db.insert(Posts).values({
-              usersId: userId,
-              title: post.title,
-              content: post.content,
-              postedAt: dayjs(post.postedAt).toDate(),
-              postedBy: post.postedBy
-            })
-
-            const postId = newPostRes[0].insertId
-
-            for (const tag of post.tags) {
-              const tagRes = await db
-                .select()
-                .from(Tags)
-                .where(eq(Tags.name, tag))
-                .execute()
-
-              let tagId = 0
-              if (tagRes.length === 0) {
-                const newTagRes = await db.insert(Tags).values({
-                  name: tag,
-                  createdBy: post.postedBy
-                })
-
-                tagId = newTagRes[0].insertId
-              } else {
-                tagId = tagRes[0].id
-              }
-
-              await db.insert(PostTags).values({
-                postsId: postId,
-                tagsId: tagId
-              })
-            }
-          }
-
-          return {
-            message: 'Success'
-          }
-        },
-        {
-          body: t.Object({
-            data: t.Array(
-              t.Object({
-                title: t.String(),
-                content: t.String(),
-                postedAt: t.String(),
-                postedBy: t.String(),
-                tags: t.Array(t.String())
-              })
-            )
-          })
-        }
-      )
       .post(
         'search',
         async ({ body }) => {
@@ -182,6 +100,88 @@ export const postController = (es: Elysia) => {
           })
         }
       )
+  )
+
+  es.post(
+    '/posts/migrate',
+    async ({ body }) => {
+      const { data } = body as IPosts.BulkBody
+
+      for (const post of data) {
+        post.postedBy
+
+        const res = await db
+          .select()
+          .from(Users)
+          .where(eq(Users.email, post.postedBy))
+          .execute()
+
+        let userId = 0
+        if (res.length === 0) {
+          const newUserRes = await db.insert(Users).values({
+            name: post.postedBy,
+            email: `${post.postedBy}@mockmail.com`,
+            password: await bcrypt.hash(post.postedBy, 10)
+          })
+
+          userId = newUserRes[0].insertId
+        } else {
+          userId = res[0].id
+        }
+
+        const newPostRes = await db.insert(Posts).values({
+          usersId: userId,
+          title: post.title,
+          content: post.content,
+          postedAt: dayjs(post.postedAt).toDate(),
+          postedBy: post.postedBy
+        })
+
+        const postId = newPostRes[0].insertId
+
+        for (const tag of post.tags) {
+          const tagRes = await db
+            .select()
+            .from(Tags)
+            .where(eq(Tags.name, tag))
+            .execute()
+
+          let tagId = 0
+          if (tagRes.length === 0) {
+            const newTagRes = await db.insert(Tags).values({
+              name: tag,
+              createdBy: post.postedBy
+            })
+
+            tagId = newTagRes[0].insertId
+          } else {
+            tagId = tagRes[0].id
+          }
+
+          await db.insert(PostTags).values({
+            postsId: postId,
+            tagsId: tagId
+          })
+        }
+      }
+
+      return {
+        message: 'Success'
+      }
+    },
+    {
+      body: t.Object({
+        data: t.Array(
+          t.Object({
+            title: t.String(),
+            content: t.String(),
+            postedAt: t.String(),
+            postedBy: t.String(),
+            tags: t.Array(t.String())
+          })
+        )
+      })
+    }
   )
 
   return es
